ad47ef4cea328c38b800aca7d1fe9789316290a4
Kudi7k <126977626+Kudi7k@users.noreply.github.com>
2025-09-12 21:05:31 +0500

    web-bot.py

--- web-bot.py
+++ web-bot.py
@@ -203,5 +203,10 @@ def index():
 
     return render_template_string(HTML_TEMPLATE)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
+
+
